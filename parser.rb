33d2cae1ba5efc4e2c5f5cf9f48b8a6f5e326053
Jaime Spacco <jspacco@knox.edu>
2017-02-20 09:00:59 -0600

    added examples to parser

--- parser.rb
+++ parser.rb
@@ -23,6 +23,7 @@ def processPlayByPlay(filename)
     # check if there is a "summary bold" class
     summary = e.css('td[class="summary bold"]')
     if summary.length == 1
+      # This happens at the end of a drive!
       puts summary.text
     else
       # get first td, which is down/distance
@@ -31,8 +32,26 @@ def processPlayByPlay(filename)
       event=normalize(e.css('td')[1].text)
       printf("%s => %s\n", downdist, event)
       # TODO: all of the work processing the down-and-distance
-      # and the event and convert it into classes to put into
+      # and the event and convert it into model classes to put into
       # the database.
+      #
+      # Example of regex matching to find the down and distance
+      # 1st and 10 at KC25
+      m = downdist.match(/(^.*) and (\d+) at ([A-Z]+\d+)/i)
+      if m != nil
+        down, dist, loc = m.captures
+        p down, dist, loc
+      end
+
+      # OK, now process all of the events, such as:
+      # N. Edlund rush for 3 yards to the KC28 (Dakotah Jones).
+      # We are going to try to match them with a regexp, one at a time
+      m = event.match(/.* rush for (\d+) yards to the ([A-Z]+\d+) .*/i)
+      if m != nil
+        yards, location = m.captures
+        p yards, location
+      end
+
     end
   end
 end
@@ -56,7 +75,10 @@ def main
 end
 
 if __FILE__ == $0
-  #main
-
+  # test on only ony play-by-play record at a time
+  # comment this out when you want to process records in bulk
   processPlayByPlay('play-by-play/2016/Knox/20161015_m72v.xml')
+
+  # Use main to run through all data that we have
+  #main
 end
